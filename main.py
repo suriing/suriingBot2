d389eb5dc9a11c2328e43236fc8d05c46d33ba8d
suriing <suriing.diary@gmail.com>
2022-11-04 12:35:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from keep_alive import keep_alive
+from discord.ext import commands
+
+bot = commands.Bot(
+    command_prefix="!",  # Change to desired prefix
+    case_insensitive=True  # Commands aren't case-sensitive
+)
+
+bot.author_id = 487258918465306634  # Change to your discord id!!!
+
+
+@bot.event
+async def on_ready():  # When the bot is ready
+    print("I'm in")
+    print(bot.user)  # Prints the bot's username and identifier
+
+
+extensions = [
+    'cogs.cog_example'  # Same name as it would be if you were importing it
+]
+
+if __name__ == '__main__':  # Ensures this is the file being ran
+    for extension in extensions:
+        bot.load_extension(extension)  # Loades every extension.
+
+keep_alive()  # Starts a webserver to be pinged.
+token = os.environ.get("token")
+bot.run(token)  # Starts the bot
