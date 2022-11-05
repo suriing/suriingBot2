b4f2e98aa7f4ce949105cad9bd42434f25345aa4
suriing <suriing.diary@gmail.com>
2022-11-05 23:18:15 +0900

    add ascii art context menu

---
+++ image2ascii.py
@@ -0,0 +1,93 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+ascii_characters_by_surface = (
+    '`^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+)
+
+
+def main():
+    image = Image.open("image.png")
+    # you can first resize the image if needed
+    # image = image.resize((width, height))
+    ascii_art = convert_to_ascii_art(image)
+    save_as_text(ascii_art)
+
+
+def image_to_ascii(file, max_width=128, max_height=128):
+    image = Image.open(file).convert("RGB")
+    resize_ratio = max(int(image.width / max_width), int(image.height / max_height))
+    resize_img = image.resize(
+        (int(image.width / resize_ratio), int(image.height / resize_ratio))
+    )
+    # resize_img = image.resize((int(image.width / resize_ratio), int(image.height / resize_ratio)), Image.Resampling.BOX)
+    # image = image.resize((width, height))
+    ascii_art = convert_to_ascii_art(resize_img)
+    return ascii_art
+
+
+def convert_to_ascii_art(image):
+    ascii_art = []
+    (width, height) = image.size
+    for y in range(0, height - 1):
+        line = ""
+        for x in range(0, width - 1):
+            px = image.getpixel((x, y))
+            line += convert_pixel_to_character(px)
+        ascii_art.append(line)
+    return ascii_art
+
+
+def convert_pixel_to_character(pixel):
+    (r, g, b) = pixel
+    pixel_brightness = r + g + b
+    max_brightness = 255 * 3
+    brightness_weight = len(ascii_characters_by_surface) / max_brightness
+    index = int(pixel_brightness * brightness_weight) - 1
+    return ascii_characters_by_surface[index]
+
+
+def save_as_text(ascii_art):
+    with open("image.txt", "w") as file:
+        for line in ascii_art:
+            file.write(line)
+            file.write("\n")
+        file.close()
+
+
+def getSize(txt, font):
+    testImg = Image.new("RGB", (1, 1))
+    testDraw = ImageDraw.Draw(testImg)
+    return testDraw.textbbox((0, 0), txt, font)
+
+
+def text_to_img(file, fontsize=11, ratio=2.14, colortheme="light"):
+    fontname = "consola.ttf"
+    text = ""
+    with open(file, "r") as f:
+        for line in f.readlines():
+            text += line
+    if colortheme == "dark":
+        colorText = "white"
+        colorOutline = "black"
+        colorBackground = "black"
+    else:
+        colorText = "black"
+        colorOutline = "white"
+        colorBackground = "white"
+    font = ImageFont.truetype(fontname, fontsize)
+    # width, height = getSize(text, font)
+    left, top, right, bottom = getSize(text, font)
+    width = abs(right - left)
+    height = abs(bottom - top)
+    img = Image.new("RGB", (width + 4, height + 4), colorBackground)
+    d = ImageDraw.Draw(img)
+    d.text((2, 2), text, fill=colorText, font=font)
+    d.rectangle((0, 0, width + 3, height + 3), outline=colorOutline)
+    img = img.resize((int(img.width), int(img.height / ratio)), Image.Resampling.BOX)
+    return img
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
+import random
+import asyncio
+
 from keep_alive import keep_alive
 import discord
 from discord import app_commands
 from discord import ui
+from image2ascii import image_to_ascii, text_to_img
 
 
 class MyClient(discord.Client):
@@ -34,6 +38,151 @@ client = MyClient(intents=intents)
 async def on_ready():  # When the bot is ready
     print("I'm in")
     print(client.user)  # Prints the bot's username and identifier
+
+
+@client.tree.command()
+async def hello(interaction: discord.Interaction):
+    """Says hello!"""
+    await interaction.response.send_message(f"Hi, {interaction.user.mention}")
+
+
+# This context menu command only works on messages
+@client.tree.context_menu(name="Image to ASCII [jpg/png → txt]")
+async def context_image_to_ascii(
+    interaction: discord.Interaction, message: discord.Message
+):
+    "이미지(jpg/png)를 아스키 아트(txt)로 변환."
+    await interaction.response.defer()
+    attach = message.attachments[0]
+    attach_ext = os.path.splitext(attach.filename)[1].lower()
+    if not (attach_ext == ".jpg" or attach_ext == ".png"):
+        await interaction.followup.send("attachment is not 'jpg' or 'png' format")
+        return
+    await attach.save(attach.filename)
+    ### UI for asking resize size
+    ascii_text = None
+    button_px50 = ui.Button(label="px50", style=discord.ButtonStyle.secondary)
+    button_px100 = ui.Button(label="px100", style=discord.ButtonStyle.primary)
+    button_px200 = ui.Button(label="px200", style=discord.ButtonStyle.secondary)
+    button_px300 = ui.Button(label="px300", style=discord.ButtonStyle.secondary)
+    resize_view = ui.View()
+    resize_view.add_item(button_px50)
+    resize_view.add_item(button_px100)
+    resize_view.add_item(button_px200)
+    resize_view.add_item(button_px300)
+
+    async def txt_px50(interaction: discord.Interaction):
+        nonlocal ascii_text
+        px = 50
+        await interaction.response.edit_message(
+            content=f"max {px}x{px} resized image will be transformed to ascii art!",
+            view=None,
+        )
+        ascii_text = image_to_ascii(attach.filename, max_width=px, max_height=px)
+
+    async def txt_px100(interaction: discord.Interaction):
+        nonlocal ascii_text
+        px = 100
+        await interaction.response.edit_message(
+            content=f"max {px}x{px} resized image will be transformed to ascii art!",
+            view=None,
+        )
+        ascii_text = image_to_ascii(attach.filename, max_width=px, max_height=px)
+
+    async def txt_px200(interaction: discord.Interaction):
+        nonlocal ascii_text
+        px = 200
+        await interaction.response.edit_message(
+            content=f"max {px}x{px} resized image will be transformed to ascii art!",
+            view=None,
+        )
+        ascii_text = image_to_ascii(attach.filename, max_width=px, max_height=px)
+
+    async def txt_px300(interaction: discord.Interaction):
+        nonlocal ascii_text
+        px = 300
+        await interaction.response.edit_message(
+            content=f"max {px}x{px} resized image will be transformed to ascii art!",
+            view=None,
+        )
+        ascii_text = image_to_ascii(attach.filename, max_width=px, max_height=px)
+
+    button_px50.callback = txt_px50
+    button_px100.callback = txt_px100
+    button_px200.callback = txt_px200
+    button_px300.callback = txt_px300
+
+    await interaction.followup.send("Please select max size.", view=resize_view)
+    ### UI end
+
+    tempfp = "ascii-" + str(random.randint(1, 999)) + ".txt"
+    while ascii_text is None:
+        await asyncio.sleep(1)
+    with open(tempfp, "w") as file:
+        for line in ascii_text:
+            file.write(line)
+            file.write("\n")
+        file.close()
+    with open(tempfp, "rb") as f:
+        txt_file = discord.File(f)
+        await interaction.followup.send(file=txt_file)
+    os.unlink(attach.filename)
+    os.unlink(tempfp)
+
+
+# This context menu command only works on messages
+@client.tree.context_menu(name="ASCII to PNG [txt → png]")
+async def context_ascii_to_image(
+    interaction: discord.Interaction, message: discord.Message
+):
+    "아스키 아트(txt)를 이미지(png)로 변환."
+    await interaction.response.defer()
+    attach = message.attachments[0]
+    attach_ext = os.path.splitext(attach.filename)[1].lower()
+    if not attach_ext == ".txt":
+        await interaction.followup.send("attachment is not 'txt' format")
+        return
+    await attach.save(attach.filename)
+    ### UI for asking color theme
+    ascii_img = None
+    button_light = ui.Button(
+        label="light", style=discord.ButtonStyle.primary, custom_id="light_theme"
+    )
+    button_dark = ui.Button(
+        label="dark", style=discord.ButtonStyle.secondary, custom_id="dark_theme"
+    )
+    color_view = ui.View()
+    color_view.add_item(button_light)
+    color_view.add_item(button_dark)
+
+    async def dark_png(interaction: discord.Interaction):
+        nonlocal ascii_img
+        await interaction.response.edit_message(
+            content="dark theme image will be replied!", view=None
+        )
+        ascii_img = text_to_img(attach.filename, colortheme="dark")
+
+    async def light_png(interaction: discord.Interaction):
+        nonlocal ascii_img
+        await interaction.response.edit_message(
+            content="light theme image will be replied!", view=None
+        )
+        ascii_img = text_to_img(attach.filename)
+
+    button_dark.callback = dark_png
+    button_light.callback = light_png
+
+    await interaction.followup.send("Please select color theme", view=color_view)
+    ### UI end
+    tempfp = "ascii-" + str(random.randint(1, 999)) + ".png"
+    while ascii_img is None:
+        await asyncio.sleep(1)
+    ascii_img.save(tempfp, optimize=True)
+    with open(tempfp, "rb") as f:
+        picture = discord.File(f)
+        await interaction.followup.send(file=picture)
+    os.unlink(attach.filename)
+    os.unlink(tempfp)
 
 
 keep_alive()  # Starts a webserver to be pinged.
